2a0497c5fc222518ffa71120cdd5f68c91429de6
Hanwei001 <hanwei.li@gmx.de>
2026-02-04 17:52:52 +0100

    Refactor VPC and subnet resources with variables

--- main.tf
+++ main.tf
@@ -1,13 +1,24 @@
 resource "aws_vpc" "dev_vpc" {
-  cidr_block = "10.0.0.0/16"
-  enable_dns_hostnames = true 
-  enable_dns_support = true
+  cidr_block           = var.vpc_cidr
+  enable_dns_support   = true
+  enable_dns_hostnames = true
 
-  tags       =  {
-    name     = "deham33"
+  tags = {
+    Name = "test-vpc"
   }
 }
-resource "aws_subnet" "public-1" {
+
+resource "aws_internet_gateway" "dev_IGW" {
+  vpc_id = aws_vpc.dev_vpc.id
+
+  tags = {
+    Name = "test-igw"
+  }
+}
+
+
+
+resource "aws_subnet" "public1" {
   vpc_id     = aws_vpc.dev_vpc.id
   cidr_block = "10.0.1.0/24"
   availability_zone = "us-west-2a"
